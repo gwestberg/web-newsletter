f77c5f449ff704c18e4b39d1c375c07a20da3b6e
Gabriella <g.westberg@hotmail.se>
2020-06-01 09:05:47 +0200

    frontend menu

--- frontend_user/src/App.js
+++ frontend_user/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Home from './components/home';
 import Nav from './components/nav';
 import Login from './components/login';
 import Register from './components/register';
@@ -13,9 +14,12 @@ function App() {
 
     <div className="App">
       <Nav />
+      <Switch>
+      <Route path="/" exact component={Home}/>
       <Route path="/login" component={Login}/>
       <Route path="/register" component={Register}/>
       <Route path="/update" component={Update}/>
+      </Switch>
     </div>
 
     </Router>
--- frontend_user/src/components/nav.js
+++ frontend_user/src/components/nav.js
@@ -1,14 +1,21 @@
 import React from 'react';
 // import './App.css';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
     <div className="nav">
       <h3>Menu</h3>
       <ul className="navlinks">
+        <Link className= "links" to='/login'>
           <li>Login</li>
+        </Link>
+        <Link className= "links" to='/register'>
           <li>Register</li>
+        </Link>
+        <Link className= "links" to='/update'>
           <li>Update</li>
+        </Link>
       </ul>
     </div>
   );
